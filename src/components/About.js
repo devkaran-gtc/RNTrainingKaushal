9d71d17e5215219d17b526a70031d45643fd303d
kaushal-gtcsys <kaushaldarji29@gmail.com>
2023-09-12 19:06:34 +0530

    add

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <h4>Version 1.0.0</h4>
+    <a href="/goback">Go Back</a>
+    </>
+    
+  )
+}
+
+export default About
---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+import { FaTimes } from "react-icons/fa";
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`event ${task.reminder ? 
+        "reminder" : ""}`}
+        onDoubleClick={() => {
+        onToggle(task.id);
+      }}
+    >
+      <h3>
+        {task.text}{" "}
+        <FaTimes
+        className="close"
+          style={{ color: "red", cursor: "pointer",alignItems:"center"}}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
